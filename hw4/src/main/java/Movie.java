c47e0ee61fe740da22184a6a5ef50f2b4a36542b
igoroalex <igoroalex@gmail.com>
2022-08-30 17:50:19 +0300

    Add Movie.java

--- hw4/src/main/java/Movie.java
+++ hw4/src/main/java/Movie.java
@@ -1,6 +1,10 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class Movie {
@@ -15,24 +19,64 @@ public class Movie {
                 '}';
     }
 
+    public static Movie getMovieFromJSON(String json) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(json, Movie.class);
+    }
+
+    public static Movie getMovieFromJSONFile(String path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(new File(path), Movie.class);
+    }
+
+    public static List<Movie> getMoviesFromJSONFile(String path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(new File(path), new TypeReference<List<Movie>>() {
+        });
+    }
+    public static void writeMoviesToJSONFile(List<Movie> movies, String path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.writeValue(new File(path), movies);
+    }
+
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
 
-        String json = "{ \"title\" : \"MIB\", \"imdb\" : \"8.1\" }";
-        Movie m1 = mapper.readValue(json, Movie.class);
+        Movie m1 = null;
+        try {
+            m1 = getMovieFromJSON("{ \"title\" : \"MIB\", \"imdb\" : \"8.1\" }");
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
         System.out.println(m1);
 
-        Movie m2 = mapper.readValue(new File("src/main/java/movie_2.json"), Movie.class);
-        System.out.println(m2);
+        try {
+            m1 = getMovieFromJSONFile("src/main/java/movie_2.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(m1);
 
-        List<Movie> movies = mapper.readValue(new File("src/main/java/movie_3.json"), new TypeReference<List<Movie>>() {
-        });
+        List<Movie> movies = null;
+        try {
+            movies = getMoviesFromJSONFile("src/main/java/movie_3.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         for (Movie ele : movies) {
             System.out.println(ele);
         }
 
-        mapper.writeValue(new File("src/main/java/movies.json"), movies);
+        try {
+            writeMoviesToJSONFile(movies, "src/main/java/movies.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getTitle() {
