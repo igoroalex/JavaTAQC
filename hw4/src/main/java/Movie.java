f658eb6816ba758172db3ddfe4de59eed7996a44
igoroalex <igoroalex@gmail.com>
2022-08-30 10:03:57 +0300

    Add CalculatorTest.java

---
+++ hw4/src/main/java/Movie.java
@@ -0,0 +1,12 @@
+//import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Movie {
+    private String title;
+    private String imdb;
+
+    public static void main(String[] args) {
+//        ObjectMapper mapper = new ObjectMapper();
+//        String json = "{ \"title\" : \"Black\", \"imdb\" : \"8.2\" }";
+//        Movie m1 = mapper.readValue(json, Movie.class);
+    }
+}
--- src/hm3/Bird.java
+++ src/hm3/Bird.java
@@ -1,8 +1,24 @@
 package hm3;
 
 public abstract class Bird {
-    private int feathers;
+    private String feathers;
     private boolean layEggs;
 
     public abstract String fly();
+
+    public String getFeathers() {
+        return feathers;
+    }
+
+    public void setFeathers(String feathers) {
+        this.feathers = feathers;
+    }
+
+    public boolean isLayEggs() {
+        return layEggs;
+    }
+
+    public void setLayEggs(boolean layEggs) {
+        this.layEggs = layEggs;
+    }
 }
