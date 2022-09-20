d6776e7dcefae7fa655f8eb2ced1958208ea06cc
igoroalex <igoroalex@gmail.com>
2022-09-20 13:19:02 +0300

    Add hw8

--- src/test/java/hw8/PracticalTask81Test.java
+++ src/test/java/hw8/PracticalTask81Test.java
@@ -1,10 +1,24 @@
 package hw8;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PracticalTask81Test {
+
+    private static Stream<Arguments> sizesRectangle() {
+        return Stream.of(
+                Arguments.of(2, 3, 6), // null strings should be considered blank
+                Arguments.of(2, 5, 10),
+                Arguments.of(2, 4, 8),
+                Arguments.of(3, 3, 9)
+        );
+    }
 
     @Test
     void squareRectangleCorrect() {
@@ -12,19 +26,12 @@ class PracticalTask81Test {
         assertEquals(8, res, "4*2=8!!");
     }
 
-//    static Stream<Arguments> sizesRectangle = Stream.of(
-//            Arguments.of(2, 3, 6), // null strings should be considered blank
-//            Arguments.of(2, 5, 10),
-//            Arguments.of(2, 4, 8),
-//            Arguments.of(3, 3, 9),
-//    );
-//
-//    @ParameterizedTest
-//    @VariableSource("sizesRectangle")
-//    void squareRectangleCorrectVariableSource(int a, int b, int expected) {
-//        int res = PracticalTask81.squareRectangle(a, b);
-//        assertEquals(expected, res);
-//    }
+    @ParameterizedTest(name = "hey-hello")
+    @MethodSource("sizesRectangle")
+    void squareRectangleCorrectVariableSource(int a, int b, int expected) {
+        int res = PracticalTask81.squareRectangle(a, b);
+        assertEquals(expected, res);
+    }
 
     @Test
     void squareRectangleNegative() {
